063b57a514fe0df5dc35e06b72b53ca00051a401
Diana Bao <dianantb@gmail.com>
2021-03-10 13:39:31 +0100

    concluded sorts

--- src/main/java/pid/exercise/sort/BubbleSort.java
+++ src/main/java/pid/exercise/sort/BubbleSort.java
@@ -4,6 +4,50 @@ public class BubbleSort {
 
     public static void sort(int[] array) {
         // TODO Implement bubble sort
+
+        int i; // current index
+        int j; // next element's index
+        int x; //temp variable
+        int a = array.length;
+
+        // Step 1. enter the loop
+        // Step 2. compare indexes
+
+        for (i = 0; i < a - 1; i++) {
+            for (j = 0; j < a - i - 1; j++)
+                if (array[j] > array[j + 1]) {
+                    // swap arr[j+1] and arr[j]
+                    x = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = x;
+                }
+        }
+    }
+}
+
+
+/*
+    int i; // current index
+    int x; //temp variable
+    int a = array.length;
+    int last = array[array.length - 1];*/
+
+
+            /*if (array[i] > array[i+1]) {
+                // do something: swap
+                x = array[i];
+                array[i] = array[i+1];
+                array[i+1] = x;
+
+                if (array[i+1] == last) {
+                    // set array.length minus 1 (decrease it by -1)
+                    a = a -1;
+                }
+                // else: do nothing
+            }
+            // the firs item is not bigger then don't swap
+            // move to the next number, so do nothing
+        }
     }
 
-}
+}*/
--- src/main/java/pid/exercise/sort/SelectionSort.java
+++ src/main/java/pid/exercise/sort/SelectionSort.java
@@ -4,6 +4,43 @@ public class SelectionSort {
 
     public static void sort(int[] array) {
         // TODO Implement selection sort
-    }
+        // TIP: start the smallest iteration first and then go broader
 
+
+        int min; // stores index of min valuable after comparison
+        int i; // current index
+        int j; // next element's index
+        int x; // temporary variable
+
+
+        // outer loop = just iterates until the list is over OR it doesnt iterate if list is empty.
+
+        // {4,6,2,8}
+
+        // traverse through the array.
+        // since i run the sort as i run the loop. The last item is already there, thefore the -1
+        for (i = 0; i < array.length-1; i++) {
+            min = i;
+            {
+                // inner loop
+                // j = i + 1 : relates to the next item
+            for (j = i + 1; j < array.length; j++) {
+                if (array[j] < array[min]) {
+                        // set min index to the index of j
+                        // in this case 6 is > than 4 , so we dont do this. Go to 'else'.
+                    min = j;
+                }
+                // store x = value of 4
+                x = array[i];
+                // set 4 = 4 // data of i == data of min
+                // swap
+                array[i] = array[min];
+                // assign stored value 4 to index min
+                array[min] = x;
+                }
+            }
+        }
+
+
+    }
 }
